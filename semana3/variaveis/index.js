517190a1ca9833790e94d8b460d6155dbbe58df7
biancasleonardo <biancasleonardo@gmail.com>
2021-04-19 14:43:01 -0300

    atividade de variáveis

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,57 @@
+// 1. 10 / 10, 5
+
+// 2. 10, 10, 10
+
+// Exercício Escrito
+
+// 1.
+
+// let nome
+// let idade
+
+// console.log (typeof nome, typeof idade)
+
+// pq não foi declarado nenhum valor nas variáveis
+
+// nome = prompt("Qual o seu nome?")
+// idade = prompt("Qual a sua idade?")
+
+// console.log (typeof nome, typeof idade)
+
+// as duas variáveis foram recebidas como string, mesmo a idade sendo um número
+
+// console.log("Olá " + nome + ", você tem " + idade + " anos")
+
+
+// 2.
+
+
+// let nome = prompt("Qual o seu nome?")
+// let idade = prompt("Qual a sua idade?")
+// let estado = prompt("Em que estado você mora?")
+// let comida = prompt("Qual a sua comida favorita?")
+// let hobby = prompt("Qual seu Hobby?")
+
+// console.log("1. Qual o seu nome?\nResposta: " + nome + "\n2. Qual a sua idade?\nResposta: " + idade + "\n3.Em que estado vc mora?\nResposta: " + estado + "\nQual a sua comida favorita?\nResposta: " + comida + "\nQual seu Hobby?\nResposta: " + hobby)
+
+
+// 3.
+
+// let comidaPreferida = ['Poke', "Sushi", "Strogonoff", "Pizza", "Caldo de Ervilha"]
+
+// console.log(comidaPreferida)
+
+// console.log("Essas são as minhas comidas preferidas:\n" + comidaPreferida[0] + "\n" + comidaPreferida[1] + "\n" + comidaPreferida[2] + "\n" + comidaPreferida[3] + "\n" + comidaPreferida[4])
+
+// comidaPreferida[1] = prompt("Qual a sua comida preferida?")
+
+// console.log("Essas são as minhas comidas preferidas:\n" + comidaPreferida[0] + "\n" + comidaPreferida[1] + "\n" + comidaPreferida[2] + "\n" + comidaPreferida[3] + "\n" + comidaPreferida[4])
+
+
+// 4.
+
+let pergunta = ["Você bebeu água hoje?", "Você gosta de café?", "Hoje choveu?"]
+
+let resposta = [true, true, false]
+
+console.log(pergunta[0] + " " + resposta[0] + "\n" + pergunta[1] + " " + resposta[1] + "\n" + pergunta[2] + " " + resposta[2])
