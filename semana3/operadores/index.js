420622a169bd374dab49d651b9001794744827e6
biancasleonardo <biancasleonardo@gmail.com>
2021-04-20 15:45:54 -0300

    exercicio aula 11

---
+++ semana3/operadores/index.js
@@ -0,0 +1,67 @@
+// Exercício 1 de iterpretação
+
+// a. false
+
+// b. false
+
+// c. false
+
+// e. boolean
+
+
+// Exercício 2 de iterpretação
+
+// a. undefined
+
+// b. null
+
+// c. 11
+
+// d. 3
+
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+// f. 9
+
+
+// Exercício 1 de escrita
+
+// let idadeUsuario = Number(prompt("Qual a sua idade?"))
+// let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo(a)?"))
+
+// console.log(`Sua idade é maior que a do seu melhor amigo? ${idadeUsuario > idadeAmigo}`)
+// console.log(idadeAmigo - idadeUsuario)
+
+
+// Exercício 2 de escrita
+
+// let numeroPar = Number(prompt("Insira um número par"))
+// console.log(numeroPar % 2)
+
+// quando o número inserido é par o resultado é sempre 0
+// quando ímpar é sempre 1
+
+
+// Exercício 3 de escrita
+
+// let listaDeTarefas = Array()
+
+// listaDeTarefas[0] = prompt("Digite sua primeira Tarefa")
+// listaDeTarefas[1] = prompt("Digite sua segunda Tarefa")
+// listaDeTarefas[2] = prompt("Digite sua terceira Tarefa")
+
+// console.log(listaDeTarefas)
+
+// let tarefaRealizada = Number(prompt("Que tarefa você já realizou? De 0 a 2"))
+
+// listaDeTarefas.splice(tarefaRealizada, 1)
+
+// console.log(listaDeTarefas)
+
+
+// Exercício 4 de escrita
+
+// let nome = prompt("Qual seu nome?")
+// let email = prompt("Qual o seu email?")
+
+// console.log(`O email ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
