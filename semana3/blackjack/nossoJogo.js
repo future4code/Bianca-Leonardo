cc7635b04dff02bd40f8db5c703e20c2025ee8c0
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-04-25 14:02:27 -0300

    conclusao do arquivo nossoJogo

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -11,10 +11,27 @@
  * 
  */
 
-console.log("Bem vindo ao jogo BlackJack")
+// console.log("Bem vindo (a) ao jogo BlackJack")
 
-if (confirm("Quer iniciar uma nova partida?")) {
-   
-} else {
-   console.log('O jogo acabou')
+// if (confirm("Quer iniciar uma nova partida?")) {
+//    let cartaUsuario1 = comprarCarta()
+//    let cartaUsuario2 = comprarCarta()
+//    let cartaComputador1 = comprarCarta()
+//    let cartaComputador2 = comprarCarta()
+
+//    let totalUsuario = cartaUsuario1.valor + cartaUsuario2.valor
+//    let totalComputador = cartaComputador1.valor + cartaComputador2.valor
+
+//    console.log(`Usuário - Cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - Pontuação ${totalUsuario}`)
+//    console.log(`Computador - Cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - Pontuação ${totalComputador}`)
+
+//    if (totalUsuario > totalComputador) {
+//       console.log('O usuário venceu!')
+//    } else if (totalUsuario < totalComputador) {
+//       console.log('O Computador venceu!')
+//    } else {
+//       console.log('Empate!')
+//    }
+// } else {
+//    console.log('O jogo acabou')
 }
