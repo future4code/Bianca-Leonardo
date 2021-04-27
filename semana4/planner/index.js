31f80a07da37c3c329ba6e1449c969d4ccdf982f
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-04-27 15:46:16 -0300

    refazendo adicionar tarefas de forma dinamica

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -3,29 +3,13 @@ function adicionarTarefa() {
     const semana = document.getElementById('dias-semana')
 
     if (tarefa.value !== "") {
-        if (semana.value === 'domingo') {
-            document.getElementById('domingo').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        } else if (semana.value === 'segunda') {
-            document.getElementById('segunda').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        } else if (semana.value === 'terca') {
-            document.getElementById('terca').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        } else if (semana.value === 'quarta') {
-            document.getElementById('quarta').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        } else if (semana.value === 'quinta') {
-            document.getElementById('quinta').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        } else if (semana.value === 'sexta') {
-            document.getElementById('sexta').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        } else if (semana.value === 'sabado') {
-            document.getElementById('sabado').innerHTML += `<p>${tarefa.value}</p>`
-            tarefa.value = ""
-        }
+        document.getElementById(`${semana.value}`).innerHTML += `<p onclick="tarefaRealizada(this)">${tarefa.value}</p>`
+        tarefa.value = ""
     } else {
         alert("Digite uma tarefa!!")
     }
+}
+
+function tarefaRealizada(elemento) {
+    elemento.style = "text-decoration: line-through"
 }
