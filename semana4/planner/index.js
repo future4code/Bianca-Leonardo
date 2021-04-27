d0b8b03922e946a96cfce94ceae326385e5932a8
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-04-27 15:55:59 -0300

    incluindo a funçao limparTarefas

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -3,7 +3,7 @@ function adicionarTarefa() {
     const semana = document.getElementById('dias-semana')
 
     if (tarefa.value !== "") {
-        document.getElementById(`${semana.value}`).innerHTML += `<p onclick="tarefaRealizada(this)">${tarefa.value}</p>`
+        document.getElementById(`${semana.value}`).innerHTML += `<p class="nova-tarefa" onclick="tarefaRealizada(this)">${tarefa.value}</p>`
         tarefa.value = ""
     } else {
         alert("Digite uma tarefa!!")
@@ -12,4 +12,12 @@ function adicionarTarefa() {
 
 function tarefaRealizada(elemento) {
     elemento.style = "text-decoration: line-through"
+}
+
+function limparTarefas() {
+    let limpar = document.getElementsByClassName('nova-tarefa')
+
+    for (let i of limpar) {
+        i.innerHTML = ''
+    }
 }
