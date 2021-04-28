86304846b5c23087edea0990596c0659bc2712ae
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-04-28 18:58:19 -0300

    desafios e estilizaçao

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,13 +1,25 @@
-function criarPost (){
+function criarPost() {
+
     let tituloPost = document.getElementById('titulo-post')
     let autorPost = document.getElementById('autor-post')
     let conteudoPost = document.getElementById('conteudo-post')
+    let imagemPost = document.getElementById('imagem-post')
     let post = document.getElementById('post')
-    
-    post.innerHTML = `<h2>${tituloPost.value}</h2> <h4>${autorPost.value}</h4> <p>${conteudoPost.value}</p>`
+
+    const postObject = {
+        titulo: tituloPost.value,
+        autor: autorPost.value,
+        conteudo: conteudoPost.value,
+        imagem: imagemPost.value
+    }
+
+
     tituloPost.value = ""
     autorPost.value = ""
     conteudoPost.value = ""
-    
-    // console.log('funcionou')
+    imagemPost.value = ""
+
+    post.innerHTML += `<h2>${postObject.titulo}</h2> <h4>${postObject.autor}</h4> <img src=${postObject.imagem}> <p>${postObject.conteudo}</p>`
+
 }
+
