c47201fdb2b01d40f8bfddb1bf14f4e74f07cde6
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-05-10 18:21:00 -0300

    removendo app.css

--- semana6/labenu-forms/src/App.js
+++ semana6/labenu-forms/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import './App.css';
 import DadosGerais from './Components/DadosGerais';
 import EnsinoSuperior from './Components/EnsinoSuperior';
 import Final from './Components/Final';
