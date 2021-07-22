0cc694d2fe6a173e2b4ce035d882b31d784ac7e3
Biancasleonardo <biancasleonardo@gmail.com>
2021-07-22 18:36:07 -0300

    criado postUser

--- semana16/toDoList/src/endpoints/postUser.ts
+++ semana16/toDoList/src/endpoints/postUser.ts
@@ -1,4 +1,30 @@
-const postUser = () => {
+import { Request, Response } from "express";
+import connection from "../connection";
+
+const postUser = async (req: Request, res: Response) => {
+
+    const { name, nickname, email } = req.body
+    let errorCode = 400
+
+    try {
+
+        if (!name || !nickname || !email) {
+            errorCode = 404
+            throw new Error("Preencha todos os campos");
+        }
+
+        await connection('Users')
+            .insert({
+                id: Date.now().toString(),
+                name,
+                nickname,
+                email
+            })
+
+        res.status(200).send('Usuário criado com Sucesso!')
+    } catch (error) {
+        res.status(errorCode).send({ message: error.message })
+    }
 };
 
 export default postUser;
