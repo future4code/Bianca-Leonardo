da31e877bb3734b3ae9d0d277c6073ce29765c85
Biancasleonardo <biancasleonardo@gmail.com>
2021-07-26 12:14:26 -0300

    exercicio 2

--- semana17/aula49 - Filtros/src/index.ts
+++ semana17/aula49 - Filtros/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors' 
 import getAllUsers from './endpoint/getAllUsers'
 import getUsersByType from './endpoint/getUsersByType'
+import getUserByOrder from './endpoint/getUserByOrder'
 
  
 
@@ -10,7 +11,7 @@ const app = express()
 app.use(express.json()) 
 app.use(cors()) 
 
-app.get('/users', getAllUsers)
+app.get('/users', getUserByOrder)
 app.get('/users/:type', getUsersByType)
 
 app.listen(3003, () => { 
