2a0d02037d378d483b63dbbd51e5f6f8f80b803d
Biancasleonardo <biancasleonardo@gmail.com>
2021-07-26 12:33:50 -0300

    exercicio 4

--- semana17/aula49 - Filtros/src/endpoint/getAllUsers.ts
+++ semana17/aula49 - Filtros/src/endpoint/getAllUsers.ts
@@ -5,9 +5,21 @@ const getAllUsers = async (req: Request, res: Response) => {
     try {
 
         const name = req.query.name || '%'
+        const sort = req.query.sort === 'name' ? 'name' : 'email'
+        const order = req.query.order === 'DESC' ? 'DESC' : 'ASC'
+        const page = Number(req.query.page)
+
+
+
+        const offset:number = 5 * (page - 1)
+
 
         const users = await connection('aula48_exercicio')
         .where('name', 'LIKE', `%${name}%`)
+        .orderBy(sort, order)
+        .limit(5)
+        .offset(offset)
+
 
         res.status(200).send(users)
     } catch (error) {
--- semana17/aula49 - Filtros/src/index.ts
+++ semana17/aula49 - Filtros/src/index.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json()) 
 app.use(cors()) 
 
-app.get('/users', getUserByPage)
+app.get('/users', getAllUsers)
 app.get('/users/:type', getUsersByType)
 
 app.listen(3003, () => { 
